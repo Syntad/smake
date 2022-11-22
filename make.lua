c333a0cfe82eb125187e43e07a84df006b14cff6
Viper <65922858+ViperTools@users.noreply.github.com>
2022-11-21 18:29:03 -0600

    Update make.lua

--- make.lua
+++ make.lua
@@ -1,26 +1,20 @@
-function smake.install()
-    print('INSTALL')
+-- options
+standard('c++2a')
+
+-- includes
+include('include')
+
+if platform.is_windows then
+    include('C:\\Lua\\include', '"C:\\Lua\\lib"', 'lua')
+elseif platform.is_osx then
+    print('TODO: mac')
+elseif platform.is_linux then
+    print('TODO: linux')
+else
+    error('Unsupported platform')
 end
 
-function smake.build()
-    -- options
-    standard('c++2a')
-    
-    -- includes
-    include('include')
-    
-    if platform.is_windows then
-        include('C:\\Lua\\include', '"C:\\Lua\\lib"', 'lua')
-    elseif platform.is_osx then
-        print('TODO: mac')
-    elseif platform.is_linux then
-        print('TODO: linux')
-    else
-        error('Unsupported platform')
-    end
-    
-    -- build
-    input('main.cpp', 'src/*.cpp')
-    output('out/smake.exe')
-    build()
-end
+-- build
+input('main.cpp', 'src/*.cpp')
+output('out/smake.exe')
+build()
